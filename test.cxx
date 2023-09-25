1e3c4480bf1a6fa4cf1b79da307de473c72d9cc4
Jeremy Stucki <dev@jeremystucki.ch>
2023-09-25 14:34:37 +0200

    Add concept

--- test.cxx
+++ test.cxx
@@ -1,12 +1,35 @@
+#include <concepts>
 #include <iostream>
 using namespace std;
 
+template<typename T>
+concept Foo = requires(T a) {
+  { a.abc() } -> std::same_as<int>;
+};
+
+template<typename T>
+void bar(T a) requires Foo<T> {
+  a.abc();
+}
+
+template<Foo T>
+void baz(T a) {
+  a.abc();
+}
+
 int main() {
-    int number;
+  int number;
 
-    cout << "Enter an integer: ";
-    cin >> number;
+  cout << "Enter an integer: ";
+  cin >> number;
 
-    cout << "You entered " << number;
-    return 0;
+  if (number > 5) {
+    cout << "Foo ";
+  } else {
+    cout << "Bar ";
+  }
+
+  cout << "You entered " << number;
+
+  return 0;
 }
